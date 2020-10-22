b9d86ec74db2039828e0621711bbbb198b459628
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-10-22 16:53:33 +0800

    save

---
+++ src/main/java/DivideTwoIntegers.java
@@ -0,0 +1,41 @@
+public class DivideTwoIntegers{
+    public static void main(String[] args) {
+        DivideTwoIntegers d = new DivideTwoIntegers();
+        int dividend1 = 10, divisor1 = 3;
+        int dividend2 = 7, divisor2 = -3;
+        int dividend3 = 1, divisor3 = 1;
+        int dividend4 = -2147483648, divisor4 = -1;
+        System.out.println(d.divide(dividend1, divisor1));
+        System.out.println(d.divide(dividend2, divisor2));
+        System.out.println(d.divide(dividend3, divisor3));
+        System.out.println(d.divide(dividend4, divisor4));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+
+    }
+
+    public int divide(int dividend, int divisor) {
+        if(dividend == 0){
+            return 0;
+        }
+        int absDividend = Math.abs(dividend);
+        int absDivisor = Math.abs(divisor);
+        int quotient = 0;
+        int sum = 0;
+        while (sum <= absDividend){
+            quotient++;
+            sum += absDivisor;
+        }
+        quotient--;
+        if(sameSymbol(dividend, divisor)){
+            return quotient;
+        }else {
+            return -quotient;
+        }
+    }
+
+    boolean sameSymbol(int a, int b){
+        return (a > 0 && b > 0) || (a < 0 && b < 0);
+    }
+}
---
+++ src/main/java/NextPermutation.java
@@ -0,0 +1,43 @@
+public class NextPermutation {
+    public static void main(String[] args) {
+        // 1,5,8,4,7,6,5,3,1   -> 4
+        // 1 5 8 5 7 6 4 3 1
+        // 1 5 8 5 1 3 4 6 7
+        int[] nums = new int[]{1,5,8,4,7,6,5,3,1};
+        NextPermutation n = new NextPermutation();
+        n.nextPermutation(nums);
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i]);
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
