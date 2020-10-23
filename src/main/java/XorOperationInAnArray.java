3c863316318b8b6a18ce3ce2330af0b32ffcd5c6
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-10-23 18:51:58 +0800

    save

---
+++ src/main/java/XorOperationInAnArray.java
@@ -0,0 +1,21 @@
+public class XorOperationInAnArray{
+    public static void main(String[] args) {
+        int a = 0, b = 2;
+        System.out.println(a ^ b);
+    }
+
+    public int xorOperation(int n, int start) {
+        if(n == 0){
+            return 0;
+        }
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = start + 2 * i;
+        }
+        int ans = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
