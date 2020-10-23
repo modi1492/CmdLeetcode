6698ec83b88df49ce643d6cf021096f67a52ff87
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-10-23 19:19:53 +0800

    save

---
+++ src/main/java/GuessNumbers.java
@@ -0,0 +1,16 @@
+public class GuessNumbers{
+    public static void main(String[] args) {
+
+    }
+
+    public int game(int[] guess, int[] answer) {
+        int length = guess.length;
+        int ans = 0;
+        for(int i = 0; i < length; i++){
+            if(guess[i] == answer[i]){
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/main/java/SingleRowKeyboard.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class SingleRowKeyboard{
+    public static void main(String[] args) {
+        SingleRowKeyboard s = new SingleRowKeyboard();
+        String k1 = "abcdefghijklmnopqrstuvwxyz";
+        String w1 = "cba";
+        System.out.println(s.calculateTime(k1,w1 ));
+        k1 = "pqrstuvwxyzabcdefghijklmno"; w1 = "leetcode";
+        System.out.println(s.calculateTime(k1,w1));
+    }
+
+    public int calculateTime(String keyboard, String word) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < keyboard.length(); i++){
+            map.put(keyboard.charAt(i), i);
+        }
+        int ans = 0;
+        int prev = 0;
+        for(int i = 0; i < word.length(); i++){
+            int index = map.get(word.charAt(i));
+            ans += Math.abs(index - prev);
+            prev = index;
+        }
+        return ans;
+    }
+}
