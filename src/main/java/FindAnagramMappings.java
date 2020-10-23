bac4dd60433709674f09bee9b442414bce334425
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-10-23 19:28:55 +0800

    save

---
+++ src/main/java/FindAnagramMappings.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindAnagramMappings{
+    public static void main(String[] args) {
+        FindAnagramMappings f = new FindAnagramMappings();
+        int[] a1 = new int[]{12, 28, 46, 32, 50};
+        int[]b1 = new int[]{50, 12, 32, 46, 28};
+
+    }
+
+    public int[] anagramMappings(int[] A, int[] B) {
+        if( A == null || B == null){
+            return new int[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < B.length; i ++){
+
+        }
+    }
+}
