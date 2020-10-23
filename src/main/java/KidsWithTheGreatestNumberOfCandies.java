4c1d8c2c5a9662ebcdceec01e32c1a4a8107e852
modi1492 <modi1492@163.com>
2020-10-23 08:28:05 +0800

    save

---
+++ src/main/java/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies{
+    public static void main(String[] args) {
+        int[]candies = new int[]{2,3,5,1,3};
+        int extraCandies = 3;
+        KidsWithTheGreatestNumberOfCandies k = new KidsWithTheGreatestNumberOfCandies();
+
+        System.out.println(k.kidsWithCandies(candies, extraCandies));
+    }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        if(candies == null){
+            return new ArrayList<>();
+        }
+        int max = Integer.MIN_VALUE;
+        for (int value : candies) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        List<Boolean> list = new ArrayList<>();
+        for (int candy : candies) {
+            if (candy + extraCandies >= max) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
---
+++ src/main/java/LeftRollString.java
@@ -0,0 +1,19 @@
+public class LeftRollString {
+    public static void main(String[] args) {
+        String s1 = "abcdefg";
+        LeftRollString l = new LeftRollString();
+        System.out.println(l.reverseLeftWords(s1,2));
+
+        String s2 = "lrloseumgh";
+        System.out.println(l.reverseLeftWords(s2,6));
+    }
+
+    public String reverseLeftWords(String s, int n) {
+        if(s == null){
+            return "";
+        }
+        String s1 = s.substring(0,n);
+        String s2 = s.substring(n,s.length());
+        return s2.concat(s1);
+    }
+}
