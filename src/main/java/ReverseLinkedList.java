37a402b4716b52900450bb53159965690dee2861
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-12-01 15:24:53 +0800

    save

---
+++ src/main/java/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+import basic.ListNode;
+
+public class ReverseLinkedList{
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return head;
+        }
+        ListNode first = head;
+        ListNode second = head.next;
+        head.next = null;
+        while (second.next != null){
+            ListNode tmp = second.next;
+            second.next = first;
+            first = second;
+            second = tmp;
+        }
+        second.next = first;
+        return second;
+    }
+}
