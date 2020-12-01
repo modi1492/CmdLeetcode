98cc665bf7c48a134b1033c4b0f8ae01b83933c0
modi1492 <modi1492@163.com>
2020-12-01 19:39:57 +0800

    save

---
+++ src/main/java/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,35 @@
+import basic.TreeNode;
+
+/**
+ * 270. 最接近的二叉搜索树值
+ */
+public class ClosestBinarySearchTreeValue{
+    public static void main(String[] args) {
+
+    }
+
+    double diff = Double.MAX_VALUE;
+
+
+    public int closestValue(TreeNode root, double target) {
+        TreeNode node = root;
+        int ans = 0;
+        while (node != null){
+            if(Math.abs(node.val - target) < diff){
+                ans = node.val;
+                diff = Math.abs(node.val - target);
+            }
+            if(node.val == target){
+                break;
+            }
+            if(node.val > target){
+                node = node.left;
+            }else {
+                node = node.right;
+            }
+        }
+        return ans;
+    }
+
+
+}
