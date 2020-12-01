b634626560bbfea07d9f9191c8527cf9f2a9680b
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-12-01 14:28:21 +0800

    save

--- src/main/java/FlippingAnImage.java
+++ src/main/java/FlippingAnImage.java
@@ -1,11 +1,34 @@
-public class FlippingAnImage{
+import java.util.Arrays;
+
+public class FlippingAnImage {
     public static void main(String[] args) {
         FlippingAnImage f = new FlippingAnImage();
         System.out.println((0 + 1) % 2);
         System.out.println((1 + 1) % 2);
+        int[][] arr = new int[][]{{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+        System.out.println(Arrays.deepToString(f.flipAndInvertImage(arr)));
     }
 
     public int[][] flipAndInvertImage(int[][] a) {
-        return new int[][]{};
+        if (a == null) {
+            return new int[0][0];
+        }
+        for (int i = 0; i < a.length; i++) {
+            int j = 0;
+            int k = a[0].length - 1;
+            // 水平反转
+            while (j < k) {
+                int tmp = a[i][j];
+                a[i][j] = a[i][k];
+                a[i][k] = tmp;
+                j++;
+                k--;
+            }
+            // 反转图片
+            for (int l = 0; l < a[0].length; l++) {
+                a[i][l] = (a[i][l] + 1) % 2;
+            }
+        }
+        return a;
     }
 }
---
+++ src/main/java/ReverseVowelsOfAString.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class ReverseVowelsOfAString {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        ReverseVowelsOfAString r = new ReverseVowelsOfAString();
+        System.out.println(r.reverseVowels(s1));
+        s1 = "leetcode";
+        System.out.println(r.reverseVowels(s1));
+    }
+
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (!isVowel(arr[i])) {
+                i++;
+                continue;
+            }
+            if (!isVowel(arr[j])) {
+                j--;
+                continue;
+            }
+            // 交换
+            char tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++;
+            j--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : arr) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o'
+                || c == 'u' || c =='A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+}
