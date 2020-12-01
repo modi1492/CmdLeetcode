3c5ff21830c593255544815a8f53a2c0178555bb
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-12-01 19:07:19 +0800

    save

---
+++ src/main/java/SearchInABinarySearchTree.java
@@ -0,0 +1,23 @@
+import basic.TreeNode;
+
+/**
+ * 700. 二叉搜索树中的搜索
+ */
+public class SearchInABinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
---
+++ src/main/java/SelfDividingNumbers.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfDividingNumbers {
+    public static void main(String[] args) {
+        SelfDividingNumbers s = new SelfDividingNumbers();
+        System.out.println(s.isSelfDividingNumber(128));
+        System.out.println(s.selfDividingNumbers(1, 22));
+    }
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (isSelfDividingNumber(i)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    boolean isSelfDividingNumber(int num) {
+        int tmp = num;
+        while (tmp != 0) {
+            int remainder = tmp % 10;
+            if (remainder == 0 || num % remainder != 0) {
+                return false;
+            }
+            tmp /= 10;
+        }
+        return true;
+    }
+}
