d03cfb1aeafa40c58144ae8ae7168d92e594b18f
modi1492 <modi1492@163.com>
2021-09-28 10:27:13 +0800

    save

--- src/main/java/NextGreaterElementII.java
+++ src/main/java/NextGreaterElementII.java
@@ -1,8 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class NextGreaterElementII {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        NextGreaterElementII n = new NextGreaterElementII();
+        int[] nums = {100, 1, 11, 1, 120, 111, 123, 1, -1, -100};
+        System.out.println(Arrays.toString(n.nextGreaterElements(nums)));
+    }
 
     public int[] nextGreaterElements(int[] nums) {
-
-        return new int[0];
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int[] res = new int[nums.length];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        int count = 2 * nums.length - 1;
+        for (int i = 0; i < count; i++) {
+            int index = i % nums.length;
+            while (!stack.isEmpty() && nums[index] > nums[stack.peek()]) {
+                res[stack.pop()] = nums[index];
+            }
+            stack.push(index);
+        }
+        return res;
     }
 }
---
+++ src/main/java/NextGreaterElementIII.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class NextGreaterElementIII {
+    public static void main(String[] args) {
+        int num = 2147483486;
+        int expect = 1585_13467;
+        System.out.println(Integer.MAX_VALUE);
+        NextGreaterElementIII n = new NextGreaterElementIII();
+        System.out.println(n.nextGreaterElement(num));
+    }
+
+    public int nextGreaterElement(int n) {
+        char[] nums = ("" + n).toCharArray();
+        int i = nums.length - 2;
+        // 寻找第一个非逆序的数字 not_reverse
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i < 0) {
+            return -1;
+        }
+        int start = i;
+        // 从最后一个开始 寻找第一个大于not_reverse的数字
+        i = nums.length - 1;
+        while (i >= 0 && nums[i] <= nums[start]) {
+            i--;
+        }
+        int stop = i;
+        swap(nums, start, stop);
+        reverse(nums, start + 1);
+        try {
+            return Integer.parseInt(new String(nums));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    /**
+     * 反转字符数组
+     *
+     * @param nums
+     * @param start
+     */
+    void reverse(char[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    /**
+     * 交换字符数组中的两个字符
+     *
+     * @param nums
+     * @param i
+     * @param j
+     */
+    void swap(char[] nums, int i, int j) {
+        char tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
